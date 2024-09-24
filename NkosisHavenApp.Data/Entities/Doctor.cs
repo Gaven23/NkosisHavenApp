e1a56ab95ecbb6ccf2a025821ecc1084bb178149
Omphile Mojahi <OMojahi@clientele.co.za>
2024-09-24 18:02:02 +0200

    Major changes

---
+++ NkosisHavenApp.Data/Entities/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace NkosisHavenApp.Data.Entities
+{
+	public class Doctor
+    {
+        public int DoctorId { get; set; }
+		public string Name { get; set; }
+		public string Specializatoon { get; set; }
+	}
+}
